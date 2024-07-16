1447a08f819a8eec66caa7e07caf0c008630b316
balkac <furkanbalkac@gmail.com>
2024-07-16 11:14:54 +0300

    CorsConfig added

--- src/main/java/com/balkac/contactsapi/constant/Constant.java
+++ src/main/java/com/balkac/contactsapi/constant/Constant.java
@@ -3,4 +3,6 @@ package com.balkac.contactsapi.constant;
 public class Constant 
 {
     public static final String PHOTO_DIRECTORY = System.getProperty("user.home") + "/Downloads/Uploads/";
+
+    public static final String X_REQUESTED_WITH = "X-Requested-With";
 }
